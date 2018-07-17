3c62b058b7a9bd85d8dccdaf3b8fd6e25caf999b
Martin <martin@coderdojoscotland.com>
2018-07-17 21:07:38 +0100

    Updated to read a line from serial and write it to csv file on exit

--- Wireless-CSV/microbit_csv.py
+++ Wireless-CSV/microbit_csv.py
@@ -1,9 +1,11 @@
-# By Andrew Mulholland - https://github.com/gbaman/microbit-experiments
+# Original by Andrew Mulholland - Forked from
+# https://github.com/gbaman/microbit-experiments
 
 import serial
 import csv
 import sys
 from serial.tools.list_ports import comports as list_serial_ports
+import atexit
 
 
 def guess_port():
@@ -14,52 +16,49 @@ def guess_port():
     """
     ports = list_serial_ports()
     platform = sys.platform
-    if platform.startswith("linux"):
+    if platform.startswith('linux'):
         for port in ports:
-            if "VID:PID=0D28:0204" in port[2].upper():
+            if 'VID:PID=0D28:0204' in port[2].upper():
                 return port[0]
-    elif platform.startswith("darwin"):
+    elif platform.startswith('darwin'):
         for port in ports:
-            if "VID:PID=0D28:0204" in port[2].upper():
+            if 'VID:PID=0D28:0204' in port[2].upper():
                 return port[0]
-    elif platform.startswith("win"):
+    elif platform.startswith('win'):
         for port in ports:
-            if "VID:PID=0D28:0204" in port[2].upper():
+            if 'VID:PID=0D28:0204' in port[2].upper():
                 return port[0]
     return None
 
 
+def close_port(port):
+    port.close()
+
+
+def write_file(data):
+    with open('microbit_data.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for val in data:
+            row = val.split()
+            writer.writerow(row)
+        print('Data saved to microbit_data.csv!')
+
+
 def main():
-    data = [["Value", "Time", "Name"], ]
-    try:
-        port = guess_port()
-        if port == None:
-            print("No micro:bit detected!")
-            sys.exit(1)
-        ser = serial.Serial(port, baudrate=115200)
-        print("Micro:bit connected and reading data from it.")
-        print("To save the data, simply close the program and it will be saved to microbit_data.csv.")
-        while True:
-            line = ser.readline().decode()
-            lineraw = line[1:len(line) - 4]
-            line = ""
-            for char in lineraw:
-                if char != "\"":
-                    line = line + char
-            line = line.split(",")
-            values = []
-            for index, value in enumerate(line):
-                if len(line) == 4 and index != 2:
-                    values.append(value.split(":")[1])
-            data.append(values)
-    finally:
-        try:
-            ser.close()
-            with open("microbit_data.csv", 'w', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerows(data)
-                print("Data saved to microbit_data.csv!")
-        except UnboundLocalError:
-            pass
+    data = []
+    atexit.register(write_file, data)
+    port = guess_port()
+    if port is None:
+        print('No micro:bit detected!')
+        sys.exit(1)
+    ser = serial.Serial(port, baudrate=115200)
+    atexit.register(close_port, ser)
+    print('Micro:bit connected and reading data from it.')
+    print('To save the data, simply close the program and it will be saved to \
+          microbit_data.csv.')
+    while True:
+        line = ser.readline().decode()
+        data.append(line)
+
 
 main()
